03d84bbd2bec43843480bf4577355c7551c25e5a
md37971 <michaeldelgado108@gmail.com>
2025-04-23 11:57:02 -0500

    Test commit

--- PHPAssignStart/assign3-starter/views/about.php
+++ PHPAssignStart/assign3-starter/views/about.php
@@ -2,6 +2,9 @@
 <main class="container">
   <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
     <h1 class="display-4">About</h1>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat neque modi atque vel adipisci autem, vero hic ipsa rem ut ea debitis asperiores cumque quis assumenda repudiandae optio, sint itaque.</p>
+    <p class="lead">This is the about section.</p>
+    <!--
+    gfewfwefwefefwfewf
+    ->>
   </div>
 </main>
